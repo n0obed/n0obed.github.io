00a5d1ac10a2bc23721d67c491dd7fa6930b6a87
Nikhil Prem Dinu <92361006+n0obed@users.noreply.github.com>
2023-10-18 09:28:41 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// var header = document.getElementsByClassName("navbar-custom")
+setInterval(alternateComputer, 10000);
+
+
+
+const nav = document.querySelector('.navbar-custom')
+const getOffset = () => {
+if(window.pageYOffset > 50){
+    nav.style.backgroundColor = 'rgba(33, 37, 41, 0.5)';
+    nav.style.transition = 'background 0.5s ease-out';
+}
+if (window.pageYOffset < 50) {
+    nav.style.backgroundColor = 'rgba(33, 37, 41, 1)';
+}
+}
+window.addEventListener('scroll', getOffset)
+
+
+// email button copy function
+function email(x) {
+	let email_var = "nikhilprem25@gmail.com";
+	navigator.clipboard.writeText(email_var);
+
+	// Alert the copied text
+	alert("Copied the text: " + email_var);
+}
+
+// alternate computer image 
+function alternateComputer() {
+	var grayimage = "./images/laptopGS.png";
+	var current = "file:///C:/Users/npremdinu2/Desktop/developing/website/n0obed.github.io-main/images/laptopGS.png"; // DELETE IT
+	var colorimage = "./images/laptop.png";
+	var imageID = document.getElementById("laptopImage");
+	if (current == imageID.src) {
+	//console.log(imageID.src);
+	imageID.src = colorimage;
+	} else {imageID.src = grayimage;}
+}
